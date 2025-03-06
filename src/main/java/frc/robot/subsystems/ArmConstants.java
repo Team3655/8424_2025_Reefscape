c44ce933f3b2d9183f1006d40dce1d4ca78149c1
driverStation <cjg7621@gmail.com>
2025-03-05 19:43:37 -0800

    Create ArmConstants

---
+++ src/main/java/frc/robot/subsystems/ArmConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+public class ArmConstants {
+    
+
+    public static double ARM_LEVEL_2 = 10;
+    public static double ARM_LEVEL_3 = 20;
+    public static double ARM_LEVEL_4 = 30;
+    public static double ARM_LEVEL_FEEDER = 40; 
+}
