54e7cd5f94faf993f249130d132d62da24d163d8
driverStation <cjg7621@gmail.com>
2025-03-13 19:20:08 -0700

    Wrist

--- src/main/java/frc/robot/subsystems/WristConstants.java
+++ src/main/java/frc/robot/subsystems/WristConstants.java
@@ -2,9 +2,9 @@ package frc.robot.subsystems;
 
 public class WristConstants {
 
-    public static double WRIST_LEVEL_2 = -4;
-    public static double WRIST_LEVEL_3 = -11;
+    public static double WRIST_LEVEL_2 = -30;
+    public static double WRIST_LEVEL_3 = -30;
     public static double WRIST_LEVEL_4 = 30;
-    public static double WRIST_LEVEL_FEEDER = -0.07;
-    public static double WRIST_LEVEL_START = -0.07;
+    public static double WRIST_LEVEL_FEEDER = 0;
+    public static double WRIST_LEVEL_START = 0;
 }
