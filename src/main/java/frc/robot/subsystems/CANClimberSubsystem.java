b243ea69b38777b240fe9aa49406850842b38784
Alex Cockroft <156022931+Yinyang258@users.noreply.github.com>
2025-03-19 15:11:21 -0400

    Removed unused

--- src/main/java/frc/robot/subsystems/CANClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANClimberSubsystem.java
@@ -1,22 +1,12 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CANClimberSubsystem extends SubsystemBase {
     
 // private final SparkMax Climber; 
 
-private final SparkMaxConfig ClimberConfig; 
-
-
 public CANClimberSubsystem() {
-
-    ClimberConfig = new SparkMaxConfig();
 
 //  Climber = new SparkMax(7, MotorType.kBrushless);
 
